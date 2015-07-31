30d5419a0d564ff89ed198c2cc715294a42882f8
Alex Vorobiev <alex.vorobiev@smartsheet.com>
2015-07-30 22:15:11 -0700

    Cleanup

--- package.js
+++ package.js
@@ -1,11 +1,13 @@
 Package.describe({
-  summary: "Login service for Smartsheet accounts",
+  summary: "Login service for Smartsheet accounts, API 2.0",
   version: "1.0.0",
   name: "smartsheet:meteor-accounts-smartsheet",
   git: "https://github.com/smartsheet-platform/meteor-accounts-smartsheet.git"
 });
 
 Package.on_use(function(api) {
+  api.versionsFrom('METEOR@1.0');
+
   api.use(['underscore', 'random']);
   api.use('accounts-base', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
